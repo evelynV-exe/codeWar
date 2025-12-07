a7bb22afcf78618ff842693103232204fa3440b8
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-12-07 21:21:45 +0700

    Add files via upload

---
+++ 8kyu/dnaStrand.py
@@ -0,0 +1,14 @@
+def DNA_strand(dna):
+    solution = ""
+    for char in dna:
+        if char == 'A':
+            solution += "T"
+        elif char == "T":
+            solution += "A"
+        elif char == "C":
+            solution += "G"
+        elif char == "G":
+            solution += "C"
+    return solution
+        
+print(DNA_strand("AAAA"))
