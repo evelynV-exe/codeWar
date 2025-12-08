5cb041f514a8523f8c01e9042be47fc6a85bd653
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-12-08 15:45:40 +0700

    Delete 8kyu/highestLowest.py

--- 8kyu/highestLowest.py
+++
@@ -1,7 +0,0 @@
-def high_and_low(numbers):
-    numlist = [int(n) for n in numbers.split()]
-    high = max(numlist)
-    low = min(numlist)
-    return f"{high} {low}"
-
-print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
