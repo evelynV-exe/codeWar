32760feec4d272f495408d22d8021cdb7e89c94c
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-12-20 19:23:15 +0700

    Update Sentence Calculator.py

--- 6 kyu/Sentence Calculator.py
+++ 6 kyu/Sentence Calculator.py
@@ -1,4 +1,4 @@
-def letter_to_number(s):
+def letters_to_numbers(s):
     total = 0
 
     for ch in s:
@@ -8,9 +8,9 @@ def letter_to_number(s):
             value = (ord(ch) - ord('A') + 1) * 2
         elif '0' <= ch <= '9':
             value = int(ch)
+        else:
+            value = 0
 
         total += value
     
     return total
-
-letter_to_number("I Love You")
